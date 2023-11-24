1bdaa3ddc0aedaafb0a2aa16e7abab72a0ad9621
talhaumar063 <tumar7161@gmail.com>
2023-11-24 16:12:49 +0530

    Update StoreLocalStorage By using Object

--- main.js
+++ main.js
@@ -21,10 +21,28 @@ function onSubmit(e) {
     emailInput.value = '';
   }
 }
+
+// Store user detail in local Storage
+// function saveToLocalStorage(e){
+//   e.preventDefault();
+//   const name = e.target.name.value;
+//   const email = e.target.email.value;
+//   localStorage.setItem('name' , name);
+//   localStorage.setItem('email' , email);
+// }
+
+// Store user detail using object in local Storage
 function saveToLocalStorage(e){
   e.preventDefault();
-  const name = e.target.name.value;
-  const email = e.target.email.value;
-  localStorage.setItem('name' , name);
-  localStorage.setItem('email' , email);
+  let Myobj = {
+      name : e.target.name.value,
+      email : e.target.email.value
+  };
+  
+  let Myobj_Serialize = JSON.stringify(Myobj);
+  
+  localStorage.setItem("Myobj" , Myobj_Serialize);
+
+  let Myobj_deserialize = JSON.parse(localStorage.getItem("Myobj"));
+  console.log(Myobj_deserialize);
 }
